1c90e64229804695a28311b92193410fb2a957b6
Daniyar5 <omurbekov_da@iuca.kg>
2026-04-16 21:29:41 +0600

    Update app.py

--- roulet/app.py
+++ roulet/app.py
@@ -11,11 +11,9 @@ class RouletteApp:
         self.page.window_height = 650
         self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         
-        # 3 жизни, 2 пули в барабане
         self.game = Game(lives=3, bullets=2)
         self.ui = UI()
 
-        # Добавляем звуки (ссылки на публичные эффекты, можно заменить на локальные файлы)
         self.shot_sound = ft.Audio(src="https://actions.google.com/sounds/v1/weapons/gunshot.ogg", autoplay=False)
         self.empty_sound = ft.Audio(src="https://actions.google.com/sounds/v1/tools/ratchet_wrench.ogg", autoplay=False)
         self.page.overlay.extend([self.shot_sound, self.empty_sound])
@@ -29,21 +27,18 @@ class RouletteApp:
         self.ui.reset_btn.on_click = self.restart
 
     def update_lives_ui(self):
-        # Отрисовка сердечек: целые и потерянные
         self.ui.lives.value = "❤️" * self.game.lives + "🖤" * (self.game.max_lives - self.game.lives)
         self.page.update()
     
     async def animate_drum(self):
-        # Отключаем кнопку во время анимации
         self.ui.shoot_btn.disabled = True
         self.ui.drum.src = "https://img.icons8.com/color/256/revolver.png"
         self.page.update()
 
-        # Плавная анимация вращения с помощью async
         for i in range(1, 15):
-            self.ui.drum.rotate.angle = i * 0.5  # Вращаем картинку
+            self.ui.drum.rotate.angle = i * 0.5
             self.page.update()
-            await asyncio.sleep(0.04) # Не блокирует интерфейс!
+            await asyncio.sleep(0.04)
         
         self.ui.drum.rotate.angle = 0
     
@@ -56,13 +51,13 @@ class RouletteApp:
 
         if result == "fatal_boom":
             self.shot_sound.play()
-            self.ui.drum.src = "https://img.icons8.com/color/256/skull.png" # Картинка черепа
+            self.ui.drum.src = "https://img.icons8.com/color/256/skull.png"
             self.ui.status.value ="🧨 BOOM! ИГРА ОКОНЧЕНА"
             self.ui.status.color = "red"
             self.show_dialog("Конец игры 🎈", "Вы потеряли все жизни!")
         elif result == "boom":
             self.shot_sound.play()
-            self.ui.drum.src = "https://img.icons8.com/color/256/explosion.png" # Картинка взрыва
+            self.ui.drum.src = "https://img.icons8.com/color/256/explosion.png"
             self.ui.status.value = "💥 Попадание! Минус жизнь."
             self.ui.status.color = "orange"
             self.ui.shoot_btn.disabled = False
